1f4c532c4793f9e171654276bb991129b064c176
John Paul Schmit <jschmit@nps.gov>
2022-01-11 16:38:28 -0500

    version 2.0 Adds latst data for all plots in each park

--- Forest_Veg_IAR_maker.R
+++ Forest_Veg_IAR_maker.R
@@ -1,15 +1,17 @@
 library(rmarkdown)
 library(purrr)
 
-Year<-2019
+Year<-2021
 
-ParksCodes<-c("ANTI", "CATO", "CHOH", "GWMP", "HAFE", "MANA", "MONO", "NACE", "PRWI", "ROCR", "WOTR")
+AllYears<-2017:2021
+
+ParksCodes<-c("ANTI")
 
 OutFiles<-paste0(ParksCodes,"-",Year,"-","IAR.html")
 
 
-RenderIAR<-function(Code,File,Year){ 
-  render(input='ForestVegIAR.Rmd',params=list(ReportPark=Code, ReportYear=Year), output_file = File)
+RenderIAR<-function(Code,File,Year,ReportAllYears){ 
+  render(input='ForestVegIAR.Rmd',params=list(ReportPark=Code, ReportYear=Year, ReportAllYears=AllYears), output_file = File)
 }
 
-walk2(.x=ParksCodes, .y=OutFiles, .f=RenderIAR, Year=Year)
+walk2(.x=ParksCodes, .y=OutFiles, .f=RenderIAR, Year=Year, ReportAllYears=AllYears)
